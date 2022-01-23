978a3e7cd86bd5cb15390a6fc56d9b217e9f23f5
seubpong.mon <pjame.fb@gmail.com>
2022-01-23 19:52:12 +0700

    Set remote dir

--- app-deploy.bash
+++ app-deploy.bash
@@ -2,11 +2,12 @@
 
 # $1 is branch name
 
+REMOTE_DIR=/home/devops
 HOST=onix-api.acd-np.its-software-services.com
 KEY_FILE=gce-dev.key
 OPTION=-o StrictHostKeyChecking=no
 
 # DO NOT cat any private key here
-scp -i ${KEY_FILE} ${OPTION} app-start.bash ${HOST}
-scp -i ${KEY_FILE} ${OPTION} custom.cfg ${HOST}
-scp -i ${KEY_FILE} ${OPTION} docker-compose ${HOST}
+scp -i ${KEY_FILE} ${OPTION} app-start.bash ${HOST}:${REMOTE_DIR}
+scp -i ${KEY_FILE} ${OPTION} custom.cfg ${HOST}:${REMOTE_DIR}
+scp -i ${KEY_FILE} ${OPTION} docker-compose ${HOST}:${REMOTE_DIR}
