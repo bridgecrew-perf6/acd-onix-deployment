efa12310e63c2963a51c8fb5dce9c08d19fb01fd
seubpong.mon <pjame.fb@gmail.com>
2022-01-23 19:49:44 +0700

    Added deployment pipeline

--- app-deploy.bash
+++ app-deploy.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # $1 is branch name
-# onix-api.acd-np.its-software-services.com
 
-ls -lrt *.key
-echo "Branch is [$1]"
+HOST=onix-api.acd-np.its-software-services.com
+KEY_FILE=gce-dev.key
+OPTION=-o StrictHostKeyChecking=no
+
+# DO NOT cat any private key here
+scp -i ${KEY_FILE} ${OPTION} app-start.bash ${HOST}
+scp -i ${KEY_FILE} ${OPTION} custom.cfg ${HOST}
+scp -i ${KEY_FILE} ${OPTION} docker-compose ${HOST}
